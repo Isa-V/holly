4406c2ce5799ffeba5d548e673f4aa7196dffdb8
Isa-V <66229178+Isa-V@users.noreply.github.com>
2022-07-17 22:02:41 -0400

    boton abrir modal, renderizar carro, punto cada 3 digitos, actualizacion array productos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 //Productos
 const listaProductos = [
+    /* tops */
     {
         id: 1,
-        nombre:"Polera", 
+        nombre:"Cárdigan largo de color combinado", 
         categoria: "Tops",
-        valor:15000,
-        imagen:"/img/img-pantalon.webp",
+        valor:15590,
+        imagen:"img/cardigan1-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/cardigan1-img-a.webp",
+            img2:"img/cardigan1-img-b.webp",
+            img3:"img/cardigan1-img-c.webp"
         },
         tallas:
         {
@@ -23,15 +24,15 @@ const listaProductos = [
     },
     {
         id:2,
-        nombre:"Camisa", 
+        nombre:"Cardigan nubes patrón", 
         categoria: "Tops", 
-        valor:150,
-        imagen:"/img/img-pantalon.webp",
+        valor:17290,
+        imagen:"img/cardigan2-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/cardigan2-img-a.webp",
+            img2:"img/cardigan2-img-b.webp",
+            img3:"img/cardigan2-img-c.webp"
         },
         tallas:
         {
@@ -45,15 +46,15 @@ const listaProductos = [
     },
     {
         id:3,
-        nombre:"Pantalón", 
-        categoria: "Bottoms", 
-        valor:200,
-        imagen:"/img/img-pantalon.webp",
+        nombre:"Cardigan flecos", 
+        categoria: "Tops", 
+        valor:15000,
+        imagen:"img/cardigan3-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/cardigan3-img-a.webp",
+            img2:"img/cardigan3-img-b.webp",
+            img3:"img/cardigan3-img-c.webp"
         },
         tallas:
         {
@@ -63,18 +64,42 @@ const listaProductos = [
             tallaL:"L",
             tallaXL:"XL"
         }
+        
     },
     {
         id:4,
-        nombre:"Falda", 
-        categoria: "Bottoms", 
-        valor:250,
-        imagen:"/img/img-pantalon.webp",
+        nombre:"Cardigan nudo hombos caidos", 
+        categoria: "Tops", 
+        valor:14000,
+        imagen:"img/cardigan4-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/cardigan4-img-a.webp",
+            img2:"img/cardigan4-img-b.webp",
+            img3:"img/cardigan4-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"XS",
+            tallaS:"S",
+            tallaM:"M",
+            tallaL:"L",
+            tallaXL:"XL"
+        }
+        
+    },
+    /* bottoms */
+    {
+        id:5,
+        nombre:"Falda plisada con cuadros", 
+        categoria: "Bottoms", 
+        valor:12900,
+        imagen:"img/bottom1-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/bottom1-img-a.webp",
+            img2:"img/bottom1-img-b.webp",
+            img3:"img/bottom1-img-c.webp"
         },
         tallas:
         {
@@ -86,16 +111,80 @@ const listaProductos = [
         }
     },
     {
-        id:5,
-        nombre:"Botín", 
-        categoria: "Zapatos", 
-        valor:300,
-        imagen:"/img/img-pantalon.webp",
+        id:6,
+        nombre:"Calza negra pitillo", 
+        categoria: "Bottoms", 
+        valor:7990,
+        imagen:"img/bottom2-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/bottom2-img-a.webp",
+            img2:"img/bottom2-img-b.webp",
+            img3:"img/bottom2-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"XS",
+            tallaS:"S",
+            tallaM:"M",
+            tallaL:"L",
+            tallaXL:"XL"
+        }
+    },
+    {
+        id:7,
+        nombre:"Falda negra botones", 
+        categoria: "Bottoms", 
+        valor:14900,
+        imagen:"img/bottom3-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/bottom3-img-a.webp",
+            img2:"img/bottom3-img-b.webp",
+            img3:"img/bottom3-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"XS",
+            tallaS:"S",
+            tallaM:"M",
+            tallaL:"L",
+            tallaXL:"XL"
+        }
+    },
+    {
+        id:8,
+        nombre:"Pantalón beige rectos", 
+        categoria: "Bottoms", 
+        valor:250,
+        imagen:"img/bottom4-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/bottom4-img-a.webp",
+            img2:"img/bottom4-img-b.webp",
+            img3:"img/bottom4-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"XS",
+            tallaS:"S",
+            tallaM:"M",
+            tallaL:"L",
+            tallaXL:"XL"
+        }
+    },
+    /* zapatos */
+    {
+        id:9,
+        nombre:"Zapatilla blanca cordones", 
+        categoria: "Zapatos", 
+        valor:22900,
+        imagen:"img/zapato1-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/zapato1-img-a.webp",
+            img2:"img/zapato1-img-b.webp",
+            img3:"img/zapato1-img-c.webp"
         },
         tallas:
         {
@@ -107,16 +196,58 @@ const listaProductos = [
         }
     },
     {
-        id:6,
-        nombre:"Bota", 
+        id:10,
+        nombre:"Bota pierna completa", 
         categoria: "Zapatos", 
-        valor:350,
-        imagen:"/img/img-pantalon.webp",
+        valor:28900,
+        imagen:"img/zapato2-img-a.webp",
         imgSecuntarias:
         {
-            img1:"/img/img-pantalon.webp",
-            img2:"/img/img-pantalon.webp",
-            img3:"/img/img-pantalon.webp"
+            img1:"img/zapato2-img-a.webp",
+            img2:"img/zapato2-img-b.webp",
+            img3:"img/zapato2-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"36",
+            tallaS:"37",
+            tallaM:"38",
+            tallaL:"39",
+            tallaXL:"40"
+        }
+    },
+    {
+        id:11,
+        nombre:"Botín negro taco cordones", 
+        categoria: "Zapatos", 
+        valor:24000,
+        imagen:"img/zapato3-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/zapato3-img-a.webp",
+            img2:"img/zapato3-img-b.webp",
+            img3:"img/zapato3-img-c.webp"
+        },
+        tallas:
+        {
+            tallaXS:"36",
+            tallaS:"37",
+            tallaM:"38",
+            tallaL:"39",
+            tallaXL:"40"
+        }
+    },
+    {
+        id:12,
+        nombre:"Zapatilla gatito cordones", 
+        categoria: "Zapatos", 
+        valor:27900,
+        imagen:"img/zapato4-img-a.webp",
+        imgSecuntarias:
+        {
+            img1:"img/zapato4-img-a.webp",
+            img2:"img/zapato4-img-b.webp",
+            img3:"img/zapato4-img-c.webp"
         },
         tallas:
         {
@@ -160,68 +291,26 @@ productTitle ? productTitle.innerText = seleccion : null;
 
 //Carrito de compras en el local storage
 let carrito = JSON.parse(localStorage.getItem("carrito")|| "[]")
+let carritoCantidad = carrito.length;
 
 //Suma el precio total
 let total = 0;
 
 //contar productos del carrito reflejarlos en el contador del carro de compra
-console.log(carrito.length)
 const carritoCounter = document.getElementById("carritoCounter");
-carritoCounter ? carritoCounter.innerText = carrito.length: carritoCounter.innerText ="0";
+carritoCounter ? carritoCounter.innerText = carritoCantidad: carritoCounter.innerText ="0";
 
 
-// encontrar el contenedor de los productos por Id
-const contenedorProductos= document.getElementById("contenedorProductos");
-
-
-//Agregar productos al contenedor solo si estamos en la pagina de productos
-
-const renderProductos = (productos, target) => {
-    let sumaProductos = "";
-    productos.map(producto => {
-        sumaProductos += `
-        <div class="col-4 productCard">
-            <img src="${producto.imagen}" id="${producto.id}" ref ="${producto.categoria}" class="productCardImg buttonCard" alt="">
-            <div class="productCardInfo">
-                <h6 class="productName">${producto.nombre}</h6>
-                <h5 class="productPrice">$${producto.valor}</h5>
-            </div>
-            <div class="d-grid gap-2">
-            <button class="btn btn-primary" type="button">Añadir al carro</button>
-            </div>
-        </div>
-        `
-    })
-    //agregar los productos al contenedor
-    target ? target.innerHTML = sumaProductos: null;
-
-    //Reconocer el producto para ver mas detalles
-    const buttonsProduct = document.querySelectorAll(".buttonCard");
-    buttonsProduct.forEach(buttonProduct => buttonProduct.addEventListener("click", productClic));
-}
-
-// identificar el producto que se debe mostrar después de hacer clic en la proxima pestaña y guardarla en el local storage para que se mantenga en la proxima pestaña
-let productShow = JSON.parse(localStorage.getItem("productShow")|| "[]")
-
-// crear funcion para hacer clic en el producto
-const productClic = (e) =>{
-    const idProducto = parseInt(e.target.getAttribute("id"));
-    const producto = listaProductos.find(producto => producto.id === idProducto);
-
-    //identificar el producto seleccionado
-    carrito.push(producto);
-    localStorage.setItem("carrito",JSON.stringify(carrito));
-    carritoCounter.innerText = carrito.length
+/* --- AGREGAR UN PUNTO CADA 3 DIGITOS A LOS PRECIOS --- */
+function preciosConPunto(valor) {
+    return valor.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
 
 
 
-// conectar la función con el array de productos y el contenedor filtrando segun la categoria seleccionada
-renderProductos(listaProductos.filter(producto=> producto.categoria==seleccion), contenedorProductos);
-
+/* ---- MENU Y FILTRADO POR CATEGORIA --- */
 //conectar con la class categoria
 const buttonCategorias = document.querySelectorAll(".categoria");
-
 
 //cambiar valor de la variable de la categoria seleccionada cuando hago clic
 const selectCategoria = (el)=> {
@@ -246,4 +335,109 @@ buttonCategorias.forEach(categoria => categoria.addEventListener("click", cambia
 
 
 
-// agregar productos en la nueva pestana
+/* --- RENDERIZAR PRODUCTOS POR CATEGORIA --- */
+
+// encontrar el contenedor de los productos por Id
+const contenedorProductos= document.getElementById("contenedorProductos");
+
+//Agregar productos al contenedor
+const renderProductos = (productos, target) => {
+    let sumaProductos = "";
+    productos.map(producto => {
+        sumaProductos += `
+        <div class="col-4 productCard">
+            <img src="${producto.imagen}" id="${producto.id}" ref ="${producto.categoria}" class="productCardImg buttonCard" alt="${producto.nombre}">
+            <div class="productCardInfo">
+                <h6 class="productName">${producto.nombre}</h6>
+                <h5 class="productPrice">$${preciosConPunto(producto.valor)}</h5>
+            </div>
+        </div>
+        `
+    })
+    //agregar los productos al contenedor
+    target ? target.innerHTML = sumaProductos: null;
+
+    //Reconocer el producto para ver mas detalles
+    const buttonsProduct = document.querySelectorAll(".buttonCard");
+    buttonsProduct.forEach(buttonProduct => buttonProduct.addEventListener("click", productClic));
+}
+
+// identificar el producto que se debe mostrar después de hacer clic en la proxima pestaña y guardarla en el local storage para que se mantenga en la proxima pestaña
+let productShow = JSON.parse(localStorage.getItem("productShow")|| "[]")
+
+// crear funcion para hacer clic en el producto
+const productClic = (e) =>{
+    const idProducto = parseInt(e.target.getAttribute("id"));
+    const producto = listaProductos.find(producto => producto.id === idProducto);
+
+    // agregar productos al carro solo si no está repedido
+    if(carrito.some(element => element.id===producto.id)){
+        const repetido = carrito.findIndex(element=> element.id === producto.id);
+        carrito[repetido].cantidad +=1;
+        carritoCantidad += 1;
+    } else{
+        carrito.push({
+            id: producto.id,
+            nombre:producto.nombre, 
+            valor:producto.valor,
+            imagen:producto.imagen,
+            cantidad:1,
+        });
+    }
+    //identificar el producto seleccionado
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+    carritoCounter.innerText = carrito.length;
+    console.log(carrito);
+    rendercarrito(carrito, contenedorCarrito);
+}
+
+// conectar la función con el array de productos y el contenedor filtrando segun la categoria seleccionada
+renderProductos(listaProductos.filter(producto=> producto.categoria==seleccion), contenedorProductos);
+
+
+
+/* --- RENDERIZAR PRODUCTOS EN EL CARRITO */
+//encontar el contenedor de productos en el carrito
+const contenedorCarrito= document.getElementById("contenedorProductosCarrito");
+
+// mostrar productos en el carrito
+const rendercarrito = (productos, target) => {
+    let sumaProductos = "";
+    productos.map(producto => {
+        sumaProductos += `
+        <div class="row carritoModal_producto">
+            <img src="${producto.imagen}" alt="${producto.nombre}" class="col-3">
+            <div class="col-7">
+                <h5 class="col-9">${producto.nombre}</h5>
+                <div class="d-flex gap-4">
+                    <h6>Talla: S</h6>
+                    <h6>$${preciosConPunto(producto.valor)} c/u</h6>
+                </div>
+                <div class="input-group-sm flex-nowrap">
+                    <input type="number" class="carritoModalCantidad" value="${producto.cantidad}">
+                </div>
+                <div class="carritoModalTotal">
+                    <h4>$${preciosConPunto(parseInt(producto.cantidad)*parseInt(producto.valor))}</h4>
+                </div>
+            </div>
+            <div class="col-2 d-flex justify-content-center align-items-start">
+                <button type="button" class="btn btn-danger" id"${producto.id}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        `
+    })
+    //agregar los productos al contenedor
+    target ? target.innerHTML = sumaProductos: null;
+}
+// renderizar los productos en el carro
+rendercarrito(carrito, contenedorCarrito);
+
+
+
